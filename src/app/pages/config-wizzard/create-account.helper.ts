c7180ea8259d426cebe02542c5d01ec1a351be7e
Jhoan Zerpa <jz_22@msn.com>
2022-07-06 19:02:16 -0400

    ajustes campos variables configuracion

--- src/app/pages/config-wizzard/create-account.helper.ts
+++ src/app/pages/config-wizzard/create-account.helper.ts
@@ -1,12 +1,12 @@
 interface ICreateAccount {
-  accountType: 'personal' | 'corporate';
+  accountType: 'usuario' | 'incubadora';
   accountTeamSize: '1-1' | '2-10' | '10-50' | '50+';
   accountName: string;
-  accountPlan: '1' | '2' | '3';
+  accountPlan: 'gratuito' | 'pago';
   businessName: string;
   businessDescriptor: string;
-  business: '1' | '2';
-  businessType: '1' | '2' | '3' | '4' | '5' | '6';
+  business: 'si' | 'no';
+  businessType: 'publico' | 'privado';
   businessDescription: string;
   businessEmail: string;
   nameOnCard: string;
@@ -18,14 +18,14 @@ interface ICreateAccount {
 }
 
 const inits: ICreateAccount = {
-  accountType: 'personal',
+  accountType: 'usuario',
   accountTeamSize: '50+',
   accountName: '',
-  accountPlan: '1',
+  accountPlan: 'gratuito',
   businessName: 'Keenthemes Inc.',
   businessDescriptor: 'KEENTHEMES',
-  business: '1',
-  businessType: '1',
+  business: 'si',
+  businessType: 'publico',
   businessDescription: '',
   businessEmail: 'corp@support.com',
   nameOnCard: 'Max Doe',
