9dc7b3c2fa6f0c8fed21c7dc18d84ec789403fe5
Jhoan Zerpa <jz_22@msn.com>
2022-07-12 12:25:25 -0400

    permiso menu

--- src/app/_metronic/layout/components/aside/aside-menu/aside-menu.component.ts
+++ src/app/_metronic/layout/components/aside/aside-menu/aside-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { environment } from '../../../../../../environments/environment';
 
 @Component({
@@ -7,10 +7,16 @@ import { environment } from '../../../../../../environments/environment';
   styleUrls: ['./aside-menu.component.scss'],
 })
 export class AsideMenuComponent implements OnInit {
+  public user: any = {};
+  public usuario: any = localStorage.getItem('usuario');
   appAngularVersion: string = environment.appVersion;
   appPreviewChangelogUrl: string = environment.appPreviewChangelogUrl;
 
-  constructor() {}
+  constructor(private ref: ChangeDetectorRef) {
+    this.user = JSON.parse(this.usuario);
+    console.log('usuario',this.user);
+    //this.ref.detectChanges();
+  }
 
   ngOnInit(): void {}
 }
