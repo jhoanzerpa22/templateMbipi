ee438dcd635c55ae1bb77a7539d3b4b6b8189ecf
Blennong <benjamin.lennong88@gmail.com>
2022-07-29 12:00:46 -0400

    ajuste mostrar todos los miembros de equipos dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -21,6 +21,9 @@ export class DashboardComponent implements OnInit {
   public iniciadoLength:number=0
   public completadoLength:number=0
 
+  public idEquiposArray:any;
+  public correoMiembrosEquipoList:any;
+
 
   constructor(private ref:ChangeDetectorRef, private _proyectsService: ProyectsService) {}
 
@@ -38,11 +41,15 @@ export class DashboardComponent implements OnInit {
           this.equipos = response;
           this.num_equipos = response.length;
           this.ref.detectChanges();
-
+          console.log(this.proyectos);
           //Arreglo de arreglo de proyectos
-          this.arrayProyectos=this.proyectos.map((element:any)=>element.equipos_equipo).map((element:any)=>element.equipo_proyecto)
-
+          this.arrayProyectos=this.proyectos.map((element:any)=>element.equipos_equipo).map((element:any)=>element.equipo_proyecto);
+          // console.log(this.arrayProyectos);
           this.estadoProyectCount(this.arrayProyectos);
+          this.idEquiposArray=this.proyectos.map((element:any)=>element.equipos_equipo).map((element:any)=>element.equipo_usuarios);
+          // console.log(this.idEquiposArray);
+          this.correoMiembrosEquipoList = this.allProyectMembersList(this.idEquiposArray);
+          console.log(this.correoMiembrosEquipoList);
         },
         (response) => {
             // Reset the form
@@ -67,6 +74,16 @@ export class DashboardComponent implements OnInit {
     // console.log(this.sinIniciarLength);
     // console.log(this.iniciadoLength);
     // console.log(this.completadoLength);
+  }
+
+  allProyectMembersList(idEquiposArray:any){
+    let correoMiembrosEquipo:any = [];
+    for (let i of idEquiposArray){
+      // console.log(i[0].correo);
+      correoMiembrosEquipo.push(i[0].correo)
+    }
+    return new Set(correoMiembrosEquipo);
+
   }
 
 }
