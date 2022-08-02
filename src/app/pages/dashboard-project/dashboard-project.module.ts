eab58247a7407c3ce265584427a22283dc100dde
Blennong <benjamin.lennong88@gmail.com>
2022-08-02 17:16:50 -0400

    componentes para tabs overview y settings

--- src/app/pages/dashboard-project/dashboard-project.module.ts
+++ src/app/pages/dashboard-project/dashboard-project.module.ts
@@ -5,9 +5,11 @@ import { RouterModule } from '@angular/router';
 import { WidgetsModule } from '../../_metronic/partials/content/widgets/widgets.module';
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { SettingsComponent } from './settings/settings.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
-  declarations: [DashboardProjectComponent],
+  declarations: [DashboardProjectComponent, SettingsComponent, OverviewComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
@@ -18,7 +20,11 @@ import { ReactiveFormsModule } from '@angular/forms';
       {
         path: ':id',
         component: DashboardProjectComponent
-      }
+      },
+      // {
+      //   path: '/settings',
+      //   component: SettingsComponent
+      // }
     ]),
     WidgetsModule,
     ReactiveFormsModule
