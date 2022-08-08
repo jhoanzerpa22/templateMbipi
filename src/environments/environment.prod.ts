16a80d1b5dc7260b3e3787c892f5b0729620800c
Jhoan Zerpa <jz_22@msn.com>
2022-08-08 18:19:09 -0400

    ajustes environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,8 @@ export const environment = {
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
   apiUrl: 'api',
-  API_G: 'https://mbipi.tresidea.cl/api/', // General
+  //API_G: 'https://mbipi.tresidea.cl/api/', // General
+  API_G: 'https://localhost:5000/api/', // General
   appThemeName: 'Metronic',
   appPurchaseUrl: 'https://1.envato.market/EA4JP',
   appHTMLIntegration: 'https://preview.keenthemes.com/metronic8/demo1/documentation/base/helpers/flex-layouts.html',
