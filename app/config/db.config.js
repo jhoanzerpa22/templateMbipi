2f062c1f6f425952c92c94b7ba6006a7c2a56897
Jhoan Zerpa <jz_22@msn.com>
2022-08-15 22:19:05 -0400

    ajustes heroku

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,16 +1,16 @@
 module.exports = {
 
   //Local
+  /*
   USER: "postgres",
   PASSWORD: "20588459jz",
-  DB: "mbipi_test",
+  DB: "mbipi_test",*/
 
   //Heroku
-  /*
   USER: "mtaligncgdmqzd",
   PASSWORD: "1ee6689d3e58a91bbb1da37be35d36191b1d3db6ba0bfa07f4e9bebb0b046e1c",
   DB: "d143lrl2dkme0e",
-  HOST: "ec2-3-225-110-188.compute-1.amazonaws.com",*/
+  HOST: "ec2-3-225-110-188.compute-1.amazonaws.com",
   //PORT: 5432,
 
   //Produccion
