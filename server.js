f109dbb9a548f8d4033caef903fee6343bcbec8c
Jhoan Zerpa <jz_22@msn.com>
2022-08-30 17:26:02 -0400

    cambio bd cpanel

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ server.listen(PORT);
   console.log(`Server is running on port ${PORT}.`);
 });*/
 
-const db = require("./app/models");
+/*const db = require("./app/models");
 // const path = require("path/posix");
 const Role = db.role;
 const User = db.user;
@@ -181,11 +181,11 @@ const Usuario = db.usuario;
 const Referencias = db.referencias;
 const ProyectosTipos = db.proyectos_tipos;
 const Metodologias = db.metodologias;
-
-db.sequelize.sync().then(() => {
+*/
+/*db.sequelize.sync().then(() => {
   console.log("Seeder db.");
   initial();
-});
+});*/
 /*db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
   initial();
