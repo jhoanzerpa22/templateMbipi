5cf99dc39d5c6fb5487c25e8aa9d8d494aeb7188
Jhoan Zerpa <jz_22@msn.com>
2022-09-09 12:56:38 -0400

    ajuste db

--- app/models/index.js
+++ app/models/index.js
@@ -1,31 +1,30 @@
 const dbConfig = require("../config/db.config.js");
 var pg = require('pg');
-pg.defaults.ssl = true;
+//pg.defaults.ssl = true;
 
 const Sequelize = require("sequelize");
 
 /*Heroku*/
-const sequelize = new Sequelize('postgres://mtaligncgdmqzd:1ee6689d3e58a91bbb1da37be35d36191b1d3db6ba0bfa07f4e9bebb0b046e1c@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d143lrl2dkme0e?sslmode=no-verify');
+//const sequelize = new Sequelize('postgres://mtaligncgdmqzd:1ee6689d3e58a91bbb1da37be35d36191b1d3db6ba0bfa07f4e9bebb0b046e1c@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d143lrl2dkme0e?sslmode=no-verify');
 
 //LOCAL
-/*
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  dialect: dbConfig.dialect,*//*
+  dialect: dbConfig.dialect,/*
   native: true,
   ssl: true, 
     dialectOptions: {
       ssl: true
     },
   operatorsAliases: false,
-  port: 5432,*//*
+  port: 5432,*/
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
     idle: dbConfig.pool.idle
   }
-});*/
+});
 
 const db = {};
 
